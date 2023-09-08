24372385aedcd41338565e415e59c79bb6e16f28
vagui <vagui@10.232.18.12>
2023-09-08 11:19:10 -0700

    Versión inicial del proyecto

---
+++ src/mx/itson/tortilla/negocio/Operacion.java
@@ -0,0 +1,23 @@
+package mx.itson.tortilla.negocio;
+/**
+ * contiene metodos para trabajar con cadenas de texto
+ * @author vagui Victor Rocha
+ */
+
+
+public class Operacion {
+    
+    /**
+     * Separa una cadena de texto en donde encuentre el caracter guion medio
+     * @param Oracion Es la oracion que sera separada en francciones
+     * @return Array de String en cuto cada componente sera un fraccion de la cadena
+     */
+    
+    public String[] separar(String Oracion){
+        
+        String [] resultado = Oracion.split("-");
+        
+        return resultado;
+    }
+    
+}
