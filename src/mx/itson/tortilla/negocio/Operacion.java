707f9d4fc74f6359ad439a03027cb27789ebae81
vagui <vagui@10.232.18.12>
2023-09-08 11:50:25 -0700

    Se agregó un comentario

--- src/mx/itson/tortilla/negocio/Operacion.java
+++ src/mx/itson/tortilla/negocio/Operacion.java
@@ -15,6 +15,8 @@ public class Operacion {
     
     public String[] separar(String Oracion){
         
+        //hola a todos jajajajaja xD
+        
         String [] resultado = Oracion.split("-");
         
         return resultado;
